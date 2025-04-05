001922eaf0584cf1335f5ebc99802d62fadb1604
Ali Heshmati <a.heshmati96@gmail.com>
2025-04-05 13:55:34 -0700

    bs_list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lowIndex = 0;
+    let highIndex = haystack.length;
 
+    do {
+        let midIndex = Math.floor(lowIndex + (highIndex - lowIndex) / 2);
+        let midValue = haystack[midIndex];
+
+        if (needle === midValue) {
+            return true;
+        } else if (needle > midValue) {
+            lowIndex = midIndex + 1;
+        } else {
+            highIndex = midIndex;
+        }
+    } while (lowIndex < highIndex);
+
+    return false;
 }
