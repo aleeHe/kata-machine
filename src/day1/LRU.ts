4356618312e9dcb384c571583ba46007e0d7992a
Ali Heshmati <a.heshmati96@gmail.com>
2025-04-08 20:58:58 -0700

    LRU

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,95 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
 export default class LRU<K, V> {
     private length: number;
+    private capacity: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    constructor() {
+    constructor(capacity: number = 10) {
+        this.length = 0;
+        this.capacity = capacity;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverseLookup = new Map<Node<V>, K>();
     }
 
     update(key: K, value: V): void {
+        let node = this.lookup.get(key);
+        if (!node) {
+            node = { value };
+            this.length += 1;
+            this.prepend(node);
+            this.trimCache();
 
-}
+            this.lookup.set(key, node);
+            this.reverseLookup.set(node, key);
+        } else {
+            this.detach(node);
+            this.prepend(node);
+            node.value = value;
+        }
+    }
+
     get(key: K): V | undefined {
+        const node = this.lookup.get(key);
+        if (!node) return undefined;
 
-}
+        this.detach(node);
+        this.prepend(node);
+
+        return node.value;
+    }
+
+    private detach(node: Node<V>): void {
+        if (node.prev) {
+            node.prev.next = node.next;
+        }
+
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+
+        if (node === this.head) {
+            this.head = node.next;
+        }
+
+        if (node === this.tail) {
+            this.tail = node.prev;
+        }
+
+        node.next = node.prev = undefined;
+    }
+
+    private prepend(node: Node<V>): void {
+        if (!this.head || !this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
+
+        this.head = node;
+    }
+
+    private trimCache(): void {
+        if (this.length <= this.capacity) return;
+
+        const tail = this.tail as Node<V>;
+        this.detach(this.tail as Node<V>);
+
+        const key = this.reverseLookup.get(tail) as K;
+        this.lookup.delete(key);
+        this.reverseLookup.delete(tail);
+
+        this.length -= 1;
+    }
 }
